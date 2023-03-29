7fd8c4d1461b085089e13e634c5c8f664b4b7a19
ASayidov <abdullajonsayidov@gmail.com>
2023-03-29 13:33:11 +0500

    added project images

--- src/portfolio/Project.js
+++ src/portfolio/Project.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Col, Container, Row } from 'reactstrap'
 import { GitHub, Link } from '@material-ui/icons'
-
+import PortProjectImg from "../img/portfolioPrject"
+import weatherImg from "../img/weatherApp"
+import toDoImg from "../imgtoDoList"
 
 function Project() {
   return (
