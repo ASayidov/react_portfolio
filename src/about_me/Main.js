31065ab7cfe9fc90bdd31860861aee88763d86b2
ASayidov <abdullajonsayidov@gmail.com>
2023-03-30 20:23:21 +0500

    updated aboteme page

--- src/about_me/Main.js
+++ src/about_me/Main.js
@@ -9,11 +9,12 @@ function Main() {
           <img src={myImage2} alt='photo aboute me' className='myImage'></img>
           <span>
             <p className='aboutmeText'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi.
+              Hi.
+              My name is Abdullajon. I'm from Fergana region Uzbekistan. I've been working as communications engineer at a mobile company 17 years. I've come to a point where I want some change.
               <div>
-                <b>Lorem ipsum dolor amet?</b>
+                <b>At the beginning of 2022 I finally fully commited to learning front-end development. </b>
               </div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
+              I created this website to present my current skills and examples of my code, which could possibly help me find my first job as a junior front-end developer.
             </p>
           </span>
         </>
