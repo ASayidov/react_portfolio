9d67d57a6f1393b407efb8334d105f1f2d458691
Abdullajon Sayidov <104810224+ASayidov@users.noreply.github.com>
2024-12-27 14:25:56 +0500

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -7,7 +7,7 @@ import './styles/responsive.scss'
 import Portfolio from "./portfolio/Portfolio";
 import About from "./about_me/About";
 import Google from "./google31fe965c0743800f.html";
-import File from "./app-ads.text";
+
 
 
 
@@ -20,7 +20,7 @@ function Routing() {
         <Route exact path="/portfolio" Component={Portfolio} />
         <Route exact path="/about-me" Component={About} />
         <Route exact path="/google31fe965c0743800f.html" Component={Google} />
-        <Route exact path="./app-ads.text" Component={File} />
+        
           
           
       </Routes>
